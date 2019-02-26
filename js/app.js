b845c50f4aa95f5496c55d2a5d3530703b2eca3f
Wryhder <jemimahsummers@yahoo.com>
2019-02-26 16:01:44 +0100

    Define variables for user search string and url to be used for API call

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
+
     const booksContainer = document.querySelector('#search-results'),
         form = document.querySelector('.form-wrapper'),
         searchInput = document.querySelector('.search-input'),
         statusDiv = document.querySelector('#status > div');
+
+    let userInput, url;
 }); 
