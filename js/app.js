d9c6671e1ae850ddc95ecc75765efc5cfc6eb253
Wryhder <jemimahsummers@yahoo.com>
2019-02-26 15:59:20 +0100

    Select DOM elements to be manipulated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
     
+    const booksContainer = document.querySelector('#search-results'),
+        form = document.querySelector('.form-wrapper'),
+        searchInput = document.querySelector('.search-input'),
+        statusDiv = document.querySelector('#status > div');
 }); 
